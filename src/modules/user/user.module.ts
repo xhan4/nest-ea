3ba2e99cca8151fccbda5410a2e0c123c588e639
hanxiao <179573450@qq.com>
2025-07-11 16:48:07 +0800

    fix:去掉多余代码#sync#

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,14 +3,9 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/user.entity';
 import { UserService } from './user.service';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from 'src/core/auth/roles.guard';
 @Module({
   imports:[TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers:[UserService,{
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    }],
+  providers:[UserService]
 })
 export class UserModule {}
