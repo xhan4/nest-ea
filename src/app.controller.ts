91fe1694b2197b24bba0858a93029e856cc8b26f
hanxiao <179573450@qq.com>
2025-08-05 17:22:11 +0800

    fix:#sync# #release#

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/time")
+  @Get("/sys_time")
   getServerTime() {
     return this.appService.getServerTime();
   }
